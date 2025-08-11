6ed1f127f9d48e2602b60b9b1cf3db4a9293bc47
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2025-08-11 18:05:02 +0800

    Finalize deploy script

--- src/areyouok_telegram/config.py
+++ src/areyouok_telegram/config.py
@@ -7,17 +7,15 @@ load_dotenv()
 CONTROLLED_ENV = ["staging", "production", "staging", "research"]
 ENV = os.getenv("ENV", "development")
 
-# Encryption salt for user keys - should be a secure random string
-USER_ENCRYPTION_SALT = os.getenv("USER_ENCRYPTION_SALT", "default-salt")
-
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
 GITHUB_SHA = os.getenv("GITHUB_SHA")
 
 LOGFIRE_TOKEN = os.getenv("LOGFIRE_TOKEN")
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-DEVELOPER_CHAT_ID = os.getenv("DEVELOPER_CHAT_ID")
-DEVELOPER_THREAD_ID = os.getenv("DEVELOPER_THREAD_ID")
+
+# Encryption salt for user keys - should be a secure random string
+USER_ENCRYPTION_SALT = os.getenv("USER_ENCRYPTION_SALT", "default-salt")
 
 PG_CONNECTION_STRING = os.getenv("PG_CONNECTION_STRING")
 
@@ -27,5 +25,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 TINYURL_API_KEY = os.getenv("TINYURL_API_KEY")
 
+DEVELOPER_CHAT_ID = os.getenv("DEVELOPER_CHAT_ID")
+DEVELOPER_THREAD_ID = os.getenv("DEVELOPER_THREAD_ID")
 CHAT_SESSION_TIMEOUT_MINS = int(os.getenv("CHAT_SESSION_TIMEOUT_MINS", "60"))  # Default to 60 minutes
 LOG_CHAT_MESSAGES = os.getenv("LOG_CHAT_MESSAGES", "false").lower() in ("true", "1", "yes")
