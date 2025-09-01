b5b6f4996507b8f03952bd847708c42948dfe733
Brian Timothy Har <62061780+btimothy-har@users.noreply.github.com>
2025-09-02 00:31:50 +0800

    Remove unused model configurations (#90)

--- src/areyouok_telegram/llms/models/__init__.py
+++ src/areyouok_telegram/llms/models/__init__.py
@@ -1,13 +1,9 @@
-from areyouok_telegram.llms.models.definitions import CHAT_GPT_5
-from areyouok_telegram.llms.models.definitions import CHAT_SONNET_3_5
 from areyouok_telegram.llms.models.definitions import CHAT_SONNET_4
 from areyouok_telegram.llms.models.definitions import ONBOARDING_SONNET_4
 from areyouok_telegram.llms.models.definitions import VALIDATOR_GPT_5_NANO
 
 __all__ = [
-    "CHAT_SONNET_3_5",
     "ONBOARDING_SONNET_4",
     "CHAT_SONNET_4",
-    "CHAT_GPT_5",
     "VALIDATOR_GPT_5_NANO",
 ]
